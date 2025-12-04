b30f3a69d578b582a384631bfe161fd4888c3051
이경민 <rudals831@gmail.com>
2025-12-04 16:31:04 +0900

    feat: 토큰 재발급,응답 DTO

---
+++ src/main/java/com/example/SentiStock_backend/auth/dto/TokenReissueRequestDto.java
@@ -0,0 +1,13 @@
+package com.example.SentiStock_backend.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenReissueRequestDto {
+
+    private String refreshToken;
+}
