cf1013f4f429f9c19880753b3d54587e1e57009b
이경민 <rudals831@gmail.com>
2025-12-04 14:16:03 +0900

    feat: 유저레포지토리 생성

--- src/main/java/com/example/SentiStock_backend/user/repository/UserRepository.java
+++ src/main/java/com/example/SentiStock_backend/user/repository/UserRepository.java
@@ -1,5 +1,12 @@
 package com.example.SentiStock_backend.user.repository;
 
-public class UserRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.SentiStock_backend.user.domain.UserEntity;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUserId(String userId);
+    boolean existsByUserId(String userId);
+    boolean existsByUserEmail(String userEmail);
 }
