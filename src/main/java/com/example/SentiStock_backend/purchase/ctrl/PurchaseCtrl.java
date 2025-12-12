a2d94a503a98003798d6db42e3077effa90abc92
정회성 <jhs9221@gmail.com>
2025-12-12 10:25:19 +0900

    feat: 주석 제거

--- src/main/java/com/example/SentiStock_backend/purchase/ctrl/PurchaseCtrl.java
+++ src/main/java/com/example/SentiStock_backend/purchase/ctrl/PurchaseCtrl.java
@@ -5,7 +5,6 @@ import com.example.SentiStock_backend.purchase.domain.dto.PurchaseRequestDto;
 import com.example.SentiStock_backend.purchase.domain.dto.PurchaseResponseDto;
 import com.example.SentiStock_backend.purchase.service.PurchaseService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -21,7 +20,7 @@ public class PurchaseCtrl {
     @Operation(summary = "사용자 매수 종목 등록", description = "사용자가 매수한 종목 정보를 등록합니다.")
     @PostMapping("/save")
     public ResponseEntity<PurchaseResponseDto> savePurchase(
-            @AuthenticationPrincipal CustomUserDetails userDetails, // ⭐ 로그인 사용자 정보
+            @AuthenticationPrincipal CustomUserDetails userDetails, 
             @RequestBody PurchaseRequestDto request) {
 
         return ResponseEntity.ok(purchaseService.savePurchase(userDetails.getId(), request));
