3dfca675317d8a8d64133e2a8bc9bb28f76d57e7
정회성 <jhs9221@gmail.com>
2025-12-23 17:23:34 +0900

    feat: kafka 세팅, 로직 구현 대기

--- src/main/java/com/example/SentiStock_backend/event/StockEvent.java
+++ src/main/java/com/example/SentiStock_backend/event/StockEvent.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString 
 public class StockEvent {
 
     private StockEventType type;
---
+++ src/main/java/com/example/SentiStock_backend/notification/ctrl/KafkaTestCtrl.java
@@ -0,0 +1,39 @@
+package com.example.SentiStock_backend.notification.ctrl;
+
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;  
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SentiStock_backend.event.StockEvent;
+import com.example.SentiStock_backend.event.StockEventProducer;
+import com.example.SentiStock_backend.event.StockEventType;
+
+import java.time.LocalDateTime;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/test")
+public class KafkaTestCtrl {
+
+    private final StockEventProducer producer;
+
+    @GetMapping("/kafka")
+    public String sendTestEvent() {
+
+        producer.publish(
+            StockEvent.builder()
+                .type(StockEventType.SENTIMENT_CHANGED)
+                .companyId("005930")
+                .sentimentScore(0.42)
+                .sentimentChange(-0.35)
+                .occurredAt(LocalDateTime.now())
+                .build()
+        );
+
+        return "Kafka event sent";
+    }
+}
