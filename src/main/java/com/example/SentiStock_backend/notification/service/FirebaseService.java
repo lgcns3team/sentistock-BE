69ab9f8e278af12953820689d8aa4d75da1cea91
정회성 <jhs9221@gmail.com>
2026-01-09 16:45:48 +0900

    fix: aws 수정

--- src/main/java/com/example/SentiStock_backend/notification/service/FirebaseService.java
+++ src/main/java/com/example/SentiStock_backend/notification/service/FirebaseService.java
@@ -3,7 +3,7 @@ package com.example.SentiStock_backend.notification.service;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
+
 
 import org.springframework.stereotype.Service;
 
@@ -14,17 +14,15 @@ public class FirebaseService {
 
         Message message = Message.builder()
                 .setToken(token)
-                .setNotification(
-                        Notification.builder()
-                                .setTitle(title)
-                                .setBody(body)
-                                .build())
+                .putData("title", title)
+                .putData("body", body)
                 .build();
 
         try {
             String response = FirebaseMessaging.getInstance().send(message);
-            System.out.println("FCM 전송 성공: " + response);
+            System.out.println("[FCM] send success messageId=" + response);
         } catch (FirebaseMessagingException e) {
+            System.err.println("[FCM] send failed");
             e.printStackTrace();
         }
     }
